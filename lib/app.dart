278e82d04ea808dbc58e9277d419bafd499d436d
AFS Consulting <AFS Consulting@DESKTOP-GHEHAFH>
2021-02-05 16:18:09 +0100

    drawers

--- lib/app.dart
+++ lib/app.dart
@@ -86,6 +86,52 @@ class AppState extends State<App> {
             ],
           ),
         ),
+        drawer: Drawer(
+          child: ListView(
+            padding: const EdgeInsets.all(0),
+            children: [
+              //DrawerHeader(
+              // child: Text('MENU', style: TextStyle(color: Colors.white)),
+              // decoration: BoxDecoration(
+              //   color: barColor,
+              // ),
+              //),
+              UserAccountsDrawerHeader(
+                accountName: Text('Timtyger'),
+                accountEmail: Text('timtyger796@gmail.com'),
+                //currentAccountPicture: Image.network('')
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(''),
+                ),
+                decoration: BoxDecoration(
+                  color: barColor,
+                ),
+                margin: const EdgeInsets.only(bottom: 0),
+              ),
+              //ListTile(
+              //  tileColor: barColor,
+              //),
+              ListTile(
+                tileColor: barColor,
+                title: Text('MENU', style: TextStyle(color: Colors.white)),
+              ),
+              ListTile(
+                selectedTileColor: barColor,
+                leading: Icon(Icons.shopping_bag, color: barColor),
+                title: Text('Shop By Category'),
+                subtitle: Text('categories'),
+                trailing: Icon(Icons.arrow_right, color: barColor),
+                onTap: () {},
+              ),
+              ListTile(
+                selectedTileColor: barColor,
+                leading: Icon(Icons.phone, color: barColor),
+                title: Text('Contact Us'),
+                trailing: Icon(Icons.arrow_right, color: barColor),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
