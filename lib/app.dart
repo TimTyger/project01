be3dff8a4bb656b593606b4b7ebdbded5d0b300a
AFS Consulting <AFS Consulting@DESKTOP-GHEHAFH>
2021-02-05 09:37:27 +0100

    commit

--- lib/app.dart
+++ lib/app.dart
@@ -14,21 +14,36 @@ class AppState extends State<App> {
 
   Widget build(context) {
     return MaterialApp(
-        home: Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: barColor,
-        child: Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            i++;
-          });
-        },
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: barColor,
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              i++;
+            });
+          },
+        ),
+        appBar: AppBar(
+          title: Text('DAVERA'),
+          backgroundColor: barColor,
+        ),
+        body: Center(
+          child: Container(
+              width: 50,
+              height: 50,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: Colors.amber,
+                  gradient:
+                      LinearGradient(colors: [Colors.amberAccent, barColor]),
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: [BoxShadow(blurRadius: 10, color: Colors.grey)]),
+              child: Text('$i',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold))),
+        ),
       ),
-      appBar: AppBar(
-        title: Text('DAVERA'),
-        backgroundColor: barColor,
-      ),
-      body: Text('$i'),
-    ));
+    );
   }
 }
