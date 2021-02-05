a9bcbdad54516b5972ae930e686f64a13f37b16b
AFS Consulting <AFS Consulting@DESKTOP-GHEHAFH>
2021-02-05 10:25:54 +0100

    rows

--- lib/app.dart
+++ lib/app.dart
@@ -29,19 +29,62 @@ class AppState extends State<App> {
           backgroundColor: barColor,
         ),
         body: Center(
-          child: Container(
-              width: 50,
-              height: 50,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: Colors.amber,
-                  gradient:
-                      LinearGradient(colors: [Colors.amberAccent, barColor]),
-                  borderRadius: BorderRadius.circular(5),
-                  boxShadow: [BoxShadow(blurRadius: 10, color: Colors.grey)]),
-              child: Text('$i',
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold))),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                      color: Colors.amber,
+                      gradient: LinearGradient(
+                          colors: [Colors.amberAccent, barColor]),
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: [
+                        BoxShadow(blurRadius: 10, color: Colors.grey)
+                      ]),
+                  alignment: Alignment.center,
+                  child: Text('$i',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20))),
+              Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                      color: Colors.amber,
+                      gradient: LinearGradient(
+                          colors: [Colors.amberAccent, barColor]),
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: [
+                        BoxShadow(blurRadius: 10, color: Colors.grey)
+                      ]),
+                  alignment: Alignment.center,
+                  child: Text('$i',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20))),
+              Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                      color: Colors.amber,
+                      gradient: LinearGradient(
+                          colors: [Colors.amberAccent, barColor]),
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: [
+                        BoxShadow(blurRadius: 10, color: Colors.grey)
+                      ]),
+                  alignment: Alignment.center,
+                  child: Text('$i',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20))),
+            ],
+          ),
         ),
       ),
     );
