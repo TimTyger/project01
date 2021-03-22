b4f00faf2d2268aa60fddc1d24b00419f55834ad
AFS Consulting <AFS Consulting@DESKTOP-GHEHAFH>
2021-03-22 16:54:58 +0100

    page navigation

---
+++ lib/Pages/login_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:project01/app.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final formKey = GlobalKey<FormState>();
+
+  final _userNameController = TextEditingController();
+
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Login Page"),
+        ),
+        body: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Image.asset(
+              "assets/fig2.png",
+              fit: BoxFit.cover,
+              color: Colors.black.withOpacity(0.7),
+              colorBlendMode: BlendMode.darken,
+            ),
+            Center(
+              child: SingleChildScrollView(
+                child: Form(
+                  key: formKey,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: <Widget>[
+                            SizedBox(
+                              height: 20,
+                            ),
+                            TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              controller: _userNameController,
+                              decoration: InputDecoration(
+                                  hintText: "Enter Email",
+                                  labelText: "UserName"),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            TextFormField(
+                              keyboardType: TextInputType.text,
+                              controller: _passwordController,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  hintText: "Enter Password",
+                                  labelText: "Password"),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                formKey.currentState.validate();
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => App()));
+                              },
+                              child: Text("Sign in"),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'pages/login_page.dart';
 
 void main() {
-  runApp(App());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
